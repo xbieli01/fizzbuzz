f85950df9f9bed098ee6d762e08eb3f0e670ced9
xbieli01 <bielik@centrum.sk>
2013-07-15 08:45:28 +0200

    added files

---
+++ fizz/fizz/FizzBuzzWriter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fizz
+{
+    class FizzBuzzWriter
+    {
+        public static void WriteToConsole()
+        {
+            Enumerable.Range(1, 100).ToList<int>().ForEach(n => Console.WriteLine(
+                (n % 3 == 0 && n % 5 == 0) ? "FizzBuzz" :
+                (n % 3 == 0) ? "Buzz" :
+                (n % 5 == 0) ? "Fizz" :
+                n.ToString()));
+        }
+    }
+}
